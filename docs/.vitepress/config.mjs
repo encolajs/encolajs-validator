49c250ae8f98814d72a5d2ca488645e523ff7b48
adrianmiu <florin_miu@yahoo.com>
2025-05-10 09:45:43 +0300

    Added GA script

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,21 @@ export default defineConfig({
   title: "EncolaJS Validator",
   description: "Documentation for EncolaJS Validator",
   base: "/validator/",
-  head: [['link', { rel: 'icon', href: '/validator/favicon.ico' }]],
+  head: [
+    ['link', { rel: 'icon', href: '/validator/favicon.ico' }],
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-4CP1E3Z3Q0',
+      },
+    ],
+    [
+      'script',
+      {},
+      "window.dataLayer = window.dataLayer || [];\\nfunction gtag(){dataLayer.push(arguments);}\\ngtag('js', new Date());\\ngtag('config', 'G-4CP1E3Z3Q0');",
+    ],
+  ],
   themeConfig: {
     search: {
       provider: 'local'
